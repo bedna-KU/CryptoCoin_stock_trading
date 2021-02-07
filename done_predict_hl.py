a3b1ffe9dc36ae0fda16acde9fb5ced8420af970
Bedna-KU <kdesi@kdesi.sk>
2021-02-07 13:16:51 +0100

    Initial commit

---
+++ done_predict_hl.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+from keras.callbacks import ModelCheckpoint, LambdaCallback
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import csv
+import json
+import time
+from numpy.lib.type_check import real
+import requests
+
+from numpy.lib.function_base import vectorize
+from tensorflow.python.keras.backend import dtype
+# Own library
+from models.model import lstm_hl
+
+# Suppress TensorFlow messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+os.system('python3 binance/download_doge_for_predict.py -x DOGEUSDT -s "24 hours 10 minutes ago UTC" -i 1m')
+
+####################################################
+# PARAMETERS
+####################################################
+INPUT_LEN = 1440
+OUTPUT_LEN = 10
+SHIFT = 10
+EPOCHS = 11
+BATCH_SIZE = 128
+FILEPATH = "weights_doge_btc.hdf5"
+# List = name : column
+DATA_CAT = {"doge" : 4, "btc" : 4}
+####################################################
+
+def sleep_anim(seconds):
+    start = time.time()
+    iteration = 0
+    symbols = ['-', '\\', '|', '/']
+    symbols = ['>    ', '>>   ', '>>>  ', '>>>> ', '>>>>>', '>>>> ', '>>>  ', '>>   ']
+    go = True
+    while go:
+        now = time.time()
+        print(symbols[iteration], str(seconds - int(now - start)).zfill(2), end="\r", flush=True)
+        time.sleep(0.3)
+        iteration += 1
+        if iteration > len(symbols) - 1:
+            iteration = 0
+        if now - start > seconds:
+            go = False
+
+def load_min_max_doge ():
+	# Read CSV file into array
+	with open ("min_max_doge.csv", newline="") as csvfile:
+		reader = csv.reader (csvfile, delimiter=',')
+		row1 = next(reader)
+		min_doge = row1[0]
+		max_doge = row1[1]
+	return min_doge, max_doge
+
+# Load last data for prefict
+def last_data_load (max_doge):
+	data_doge_raw = []
+	# Read CSV file into array
+	with open ("doge_for_predict.csv", newline="") as csvfile:
+		reader = csv.reader (csvfile, delimiter=',')
+		for row in reader:
+			data_doge_raw.append (row)
+
+	data_high_doge = get_column(data_doge_raw, 2)
+	data_low_doge = get_column(data_doge_raw, 3)
+	data_close_doge = get_column(data_doge_raw, 4)
+
+	data_high_doge = data_high_doge[ : 1440]
+	data_low_doge = data_low_doge[ : 1440]
+
+	data_real = data_close_doge[1440 : ]
+
+	data_high_doge_rows_count = len(data_high_doge)
+	print (">>> data doge rows count:", data_high_doge_rows_count)
+	print (">>> data real rows count:", len(data_real))
+
+	input_high_doge = np.array(data_high_doge)
+	input_low_doge = np.array(data_low_doge)
+
+	encode2 = np.vectorize(encode)
+	input_high_doge = encode2(data_high_doge, max_doge)
+	input_low_doge = encode2(data_low_doge, max_doge)
+
+	X = np.array(([input_high_doge], [input_low_doge]), dtype = float)
+
+	X = np.reshape(X, (1, 2, INPUT_LEN))
+	print("X", X)
+	print("X.shape", X.shape)
+	return X, data_real
+
+def get_column(matrix, i):
+	return [row[i] for row in matrix]
+
+# Invert encoding
+def decode (value, max):
+	return value * max
+
+# Encode data
+def encode (value, max):
+	result = float(value) / max
+	return result
+
+# Load trained weights
+def model_load (model):
+	if os.path.exists (FILEPATH):
+		model.load_weights (FILEPATH)
+
+def predict (X):
+	# X = np.expand_dims (X, axis = 0)
+	print(">>> X.shape", X.shape)
+	result = model.predict (X, batch_size = BATCH_SIZE, verbose = 1)
+	return result
+
+min_doge, max_doge = load_min_max_doge()
+min_doge, max_doge = float(min_doge), float(max_doge)
+
+print("max_doge", max_doge)
+
+model = lstm_hl (INPUT_LEN, OUTPUT_LEN)
+model_load(model)
+
+X, data_real = last_data_load(max_doge)
+
+start_point = decode(X[0][0][1439], max_doge)
+print(">>> start_point", start_point)
+result = predict(X)
+result_decoded = decode(result[0], max_doge)
+result_decoded = np.insert(result_decoded, 0, start_point)
+print("RESULT", result_decoded)
+data_real = np.array(data_real, dtype = float)
+data_real = np.insert(data_real, 0, start_point)
+print("REAL", data_real)
+
+plt.plot(data_real, c = 'green')
+plt.plot(result_decoded, c = 'blue')
+plt.show()
---
+++ done_train_hl.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+from keras.callbacks import ModelCheckpoint, LambdaCallback
+import numpy as np
+import os
+import sys
+import csv
+from numpy.core.defchararray import encode
+
+from numpy.lib.function_base import vectorize
+# Own library
+from models.model import lstm_hl
+
+from datetime import datetime
+
+# Suppress TensorFlow messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+####################################################
+# PARAMETERS
+####################################################
+INPUT_LEN = 1440
+OUTPUT_LEN = 10
+SHIFT = 10
+EPOCHS = 25
+BATCH_SIZE = 128
+FILEPATH = "weights_hl.hdf5"
+####################################################
+
+# Load data
+def data_load ():
+	data = {}
+
+	# Read CSV file into array
+	data = []
+	with open ("file_new.csv", newline = "") as csvfile:
+		reader = csv.reader (csvfile, delimiter = ',')
+		for row in reader:
+			data.append (row)
+
+	data_rows_count = len(data)
+	print (">>> data rows count:", data_rows_count)
+
+	doge_max = 0
+	doge_min = 999999999
+	for row in data:
+		if float(row[2]) > doge_max:
+			doge_max = float(row[2])
+		if float(row[3]) < doge_min:
+			doge_min = float(row[3])
+	print (">>> DOGE min", doge_min)
+	print (">>> DOGE max", doge_max)
+
+	with open('min_max_doge.csv', 'w') as f:
+		write = csv.writer(f, delimiter=',')
+		csv_out = [doge_min, doge_max]
+		write.writerow(csv_out)
+		print("Save min max")
+
+	# Get data from columns
+	data_time_doge = get_column(data, 0)
+	data_high_doge = get_column(data, 2)
+	data_low_doge = get_column(data, 3)
+	data_close_doge = get_column(data, 4)
+
+	input_high_doge_arr = []
+	input_low_doge_arr = []
+	output_close_doge_arr = []
+	loop = 0
+	yes = True
+	while yes:
+		input_high_doge = data_high_doge[0 + SHIFT * loop : INPUT_LEN + SHIFT * loop]
+		input_low_doge = data_low_doge[0 + SHIFT * loop : INPUT_LEN + SHIFT * loop]
+		output_close_doge = data_close_doge[INPUT_LEN + SHIFT * loop : INPUT_LEN + OUTPUT_LEN + SHIFT * loop]
+		if len(input_high_doge) < INPUT_LEN or len(output_close_doge) < OUTPUT_LEN:
+			yes = False
+		else:
+			input_high_doge_arr.append(input_high_doge)
+			input_low_doge_arr.append(input_low_doge)
+			output_close_doge_arr.append(output_close_doge)
+			loop += SHIFT
+
+	input_high_doge_arr = np.array(input_high_doge_arr)
+	input_low_doge_arr = np.array(input_low_doge_arr)
+	print(">>> count list", len(input_high_doge_arr))
+
+	encode2 = np.vectorize(encode)
+	input_high_doge_arr = encode2(input_high_doge_arr, doge_max)
+	input_low_doge_arr = encode2(input_low_doge_arr, doge_max)
+
+	X = np.array([input_high_doge_arr[0], input_low_doge_arr[0]])
+	for index in range(1, len(input_high_doge_arr)):
+		print(index)
+		X=np.append(X, [input_high_doge_arr[index], input_low_doge_arr[index]], axis = 0)
+	X = np.reshape(X, (len(input_high_doge_arr),2 ,INPUT_LEN))
+
+	print(X)
+	y = np.array(output_close_doge_arr, dtype = float)
+	encode2 = np.vectorize(encode)
+	X = encode2(X, doge_max)
+	y = encode2(y, doge_max)
+	print(y)
+	return X, y
+
+def get_column(matrix, i):
+	return [row[i] for row in matrix]
+
+# Unvectorize data
+def one_hot_decode (seq, max):
+	strings = list ()
+	for pattern in seq:
+		string = str (np.argmax (pattern))
+		strings.append (string)
+	return ' '.join (strings)
+
+# Vectorize data
+def one_hot_encode (value, max_int):
+	max_int = max_int + 1
+	value_enc = list ()
+	for seq in value:
+		pattern = list ()
+		for index in seq:
+			vector = [0 for _ in range (max_int)]
+			vector[index] = 1
+			pattern.append (vector)
+		value_enc.append (pattern)
+	return value_enc
+
+# Invert encoding
+def decode (value, max):
+	return value * max
+
+# Encode data
+def encode (value, max):
+	result = float(value) / max
+	return result
+
+# Run every epoch
+def on_epoch_end (epoch, logs):
+	print (">>>LOGS>>>", logs)
+
+# Load trained weights
+def model_load (model):
+	if os.path.exists (FILEPATH):
+		model.load_weights (FILEPATH)
+
+# Train model
+def model_train (model, X, y):
+	checkpoint = ModelCheckpoint (FILEPATH, monitor = 'loss',
+								 verbose = 1, save_best_only = True,
+								 mode = 'min')
+
+	print_callback = LambdaCallback (on_epoch_end = on_epoch_end)
+	callbacks = [print_callback, checkpoint]
+	model.fit (X, y, batch_size = BATCH_SIZE, epochs = EPOCHS, callbacks = callbacks)
+
+model = lstm_hl (INPUT_LEN, OUTPUT_LEN)
+model_load (model)
+print (model.summary())
+X, y = data_load ()
+model_train (model, X, y)
